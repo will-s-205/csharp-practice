2189e9ffd357e2f7e09c9cba54f339fea2c03247
William Step <william.step.0205@gmail.com>
2026-07-04 21:54:56 -0700

    add sum of scores to each student

--- sumOfScores/sumOfScores.cs
+++ sumOfScores/sumOfScores.cs
@@ -31,10 +31,10 @@ public class Program
         int jeong4 = 100;
         int jeong5 = 97;
 
-        int sophiaSum = 0;
-        int nicolasSum = 0;
-        int zahirahSum = 0;
-        int jeongSum = 0;
+        int sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
+        int nicolasSum = nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5;
+        int zahirahSum = zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5;
+        int jeongSum = jeong1 + jeong2 + jeong3 + jeong4 + jeong5;
 
         Console.WriteLine("Sophia: " + sophiaSum);
         Console.WriteLine("Nicolas: " + nicolasSum);
